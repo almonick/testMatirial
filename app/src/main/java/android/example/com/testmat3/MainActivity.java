62ed59d6490d92032b60f0cb6ed82b4a29e0b60d
almonick <almonick72@gmail.com>
2015-07-17 19:14:11 +0300

    test commit

---
+++ app/src/main/java/android/example/com/testmat3/MainActivity.java
@@ -0,0 +1,118 @@
+package android.example.com.testmat3;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    FloatingActionButton fab;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main1);
+
+        setupNavigationView();
+        setupToolbar();
+        setupTablayout();
+        setupCollapsingToolbarLayout();
+        setupFab();
+    }
+    private void setupCollapsingToolbarLayout(){
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        if(collapsingToolbarLayout != null){
+            collapsingToolbarLayout.setTitle(toolbar.getTitle());
+//            collapsingToolbarLayout.setCollapsedTitleTextColor(0xEDFF24);
+//            collapsingToolbarLayout.setExpandedTitleColor(0xED1CFF);
+        }
+    }
+
+    private void setupNavigationView(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null)
+            setSupportActionBar(toolbar);
+
+        // Show menu icon
+        final ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupFab(){
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        if(fab != null)
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar
+                            .make(findViewById(R.id.coordinatorLayout), "This is Snackbar", Snackbar.LENGTH_LONG)
+                            .setAction("Action", this)
+                            .show(); // Don�t forget to show!
+                }
+            });
+    }
+
+    private void setupTablayout(){
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        if(tabLayout == null)
+            return;
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if(drawerLayout != null)
+                    drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
